665ea5ad085f6471a9876739ff343413fd60b772
Nathan Randle <nrandle@dolbz.com>
2020-11-19 08:05:01 +0000

    Initial work on the gate array so that the ROMs can be enabled/disabled. Boot screen renders almost correctly now

--- CPCSharp.Core/CPCRunner.cs
+++ CPCSharp.Core/CPCRunner.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using System.IO;
 using System;
 using Z80;
 using System.Threading;
 using System.Reflection;
+using System.Collections.Generic;
 
 namespace CPCSharp.Core
 {
@@ -10,15 +12,15 @@ namespace CPCSharp.Core
         private static bool _cpuRunning = false;
         private Z80Cpu _cpu = new Z80Cpu();
         private byte[] _ram = new byte[64*1024];
+
+        private byte[] _lowerRom = new byte[16*1024];
+        private byte[] _upperRom = new byte[16*1024];
+
+        private GateArray _gateArray = new GateArray();
+
+        private List<IODevice> _ioDevices = new List<IODevice>();
         private readonly static object cpuState = new object();
         
-        public CPCRunner() {
-            // TODO shouldn't do this in a construction
-            _cpu.Initialize();
-            ThreadStart work = RunCpu;
-            Thread thread = new Thread(work);
-            thread.Start();
-        }
         public void AccessCpuState(Action<Z80Cpu, byte[]> cpuAction) {
             lock(cpuState) {
                 cpuAction(_cpu, _ram);
@@ -26,20 +28,44 @@ namespace CPCSharp.Core
         }
 
         public void Initialize() {
-            LoadROM();
+            LoadROMs();
+            SetupIODevices();
+
+            _cpu.Initialize();
+            ThreadStart work = RunCpu;
+            Thread thread = new Thread(work);
+            thread.Start();
         }
 
-        private  void LoadROM() {
+        private void LoadROMs() {
             var offset = 0;
             var assembly = typeof(CPCRunner).GetTypeInfo().Assembly;
              
             using (var romStream = assembly.GetManifestResourceStream("CPCSharp.Core.OS_464.ROM")) {
                 while (offset < romStream.Length) {
                     byte membyte = (byte)romStream.ReadByte();
-                    _ram[offset++] = membyte;
+                    _lowerRom[offset++] = membyte;
                 }
-                //Console.WriteLine($"Wrote {offset - startOffset} bytes to RAM");
+            }
+            using (var romStream = assembly.GetManifestResourceStream("CPCSharp.Core.BASIC_1.0.ROM")) {
+                while (offset < romStream.Length) {
+                    byte membyte = (byte)romStream.ReadByte();
+                    _upperRom[offset++] = membyte;
+                }
             }            
+        }
+
+        private void SetupIODevices() {
+            _ioDevices.Add(_gateArray);
+        }
+
+        private IODevice GetIoDeviceForAddress(ushort address) {
+            foreach (var device in _ioDevices) {
+                if (device.ActiveAtAddress(address)) {
+                    return device;
+                }
+            }
+            return null;
         }
 
         public void RunCpu() {
@@ -52,9 +78,28 @@ namespace CPCSharp.Core
                 lock(cpuState) {
                     do {
                         _cpu.Clock();
+                        if (_cpu.IORQ) {
+                            var ioDevice = GetIoDeviceForAddress(_cpu.Address);
+                            if (ioDevice != null) {
+                                if (_cpu.RD) {
+                                    _cpu.Data = ioDevice.Data;
+                                }
+                                else if (_cpu.WR) {
+                                    // write to an IO device
+                                    ioDevice.Data = _cpu.Data;
+                                }
+                            } else {
+                                Console.WriteLine($"IORQ for unknown IO address: {_cpu.Address:x4}");
+                            }
+                        }
                         if (_cpu.MREQ && _cpu.RD) {
-                            var data = _ram[_cpu.Address];
-                            _cpu.Data = data;
+                            if (_gateArray.LowerROMEnabled && _cpu.Address < 0x4000) {
+                                _cpu.Data = _lowerRom[_cpu.Address];
+                            } else if (_gateArray.UpperROMEnabled && _cpu.Address >= 0xc000) {
+                                _cpu.Data = _upperRom[_cpu.Address & 0x0fff];
+                            } else {
+                                _cpu.Data = _ram[_cpu.Address];;
+                            }
                         }
                         if (_cpu.MREQ && _cpu.WR) {
                             _ram[_cpu.Address] = _cpu.Data;
---
+++ CPCSharp.Core/GateArray.cs
@@ -0,0 +1,67 @@
+using System;
+
+namespace CPCSharp.Core {
+
+    public interface IODevice {
+        byte Data { get; set; }
+        bool ActiveAtAddress(ushort address);
+    }
+    public class GateArray : IODevice
+    {
+
+        public bool LowerROMEnabled { get; set;} = true;
+        public bool UpperROMEnabled { get; set;} = true;
+        private byte _data;
+        public byte Data { 
+            get { 
+                Console.WriteLine("Data read from gate array which isn't possible");
+                return 0;}
+        
+            set {
+                _data = value;
+                ProcessInputData();
+            } 
+        }
+
+        public bool ActiveAtAddress(ushort address)
+        {
+            // Returns true if most significant 2 bits == 01
+            return (address & 0xc000) == 0x4000;
+        }
+
+        private void ProcessInputData() {
+            var functionSelectionBits = _data >> 6;
+
+            switch (functionSelectionBits) {
+                case 0:
+                    // select pen
+                    break;
+                case 1:
+                    // select colour for pen
+                    break;
+                case 2:
+                    // select screen mode, ROM configuration and interrupt control
+                    ScreenModeROMConfig();
+                    break;
+                case 3:
+                    // RAM management
+                    break;
+            }
+        }
+
+        private void ScreenModeROMConfig() {
+            // 4	x	Interrupt generation control
+            // 3	x	1=Upper ROM area disable, 0=Upper ROM area enable
+            // 2	x	1=Lower ROM area disable, 0=Lower ROM area enable
+            // 1	x	Screen Mode slection
+            // 0   x
+
+            var lowerROMConfigMask = 0x4;
+            var upperROMConfigMask = 0x8;
+
+            LowerROMEnabled = (_data & lowerROMConfigMask) == 0;
+            UpperROMEnabled = (_data & upperROMConfigMask) == 0;
+            Console.WriteLine($"Screen Mode and ROM config Lower ROM En: {LowerROMEnabled}, Upper ROM EN: {UpperROMEnabled}");
+        }
+    }
+}
