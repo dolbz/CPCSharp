d347de22af0222fec8559eab8a7fea056f951349
Nathan Randle <nrandle@dolbz.com>
2021-03-22 07:46:52 +0000

    Copyright all of the things

--- CPCSharp.Core/PSG/DefaultPSG.cs
+++ CPCSharp.Core/PSG/DefaultPSG.cs
@@ -1,3 +1,7 @@
+//  
+// Copyright (c) 2021, Nathan Randle. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+// 
 ﻿namespace CPCSharp.Core.PSG
 {
     /// <summary>
--- NativePSGs/Mac/MacPSG/MacPSG/MacPSG-Bridging-Header.h
+++ NativePSGs/Mac/MacPSG/MacPSG/MacPSG-Bridging-Header.h
@@ -1,3 +1,7 @@
+//  
+// Copyright (c) 2021, Nathan Randle. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+// 
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
